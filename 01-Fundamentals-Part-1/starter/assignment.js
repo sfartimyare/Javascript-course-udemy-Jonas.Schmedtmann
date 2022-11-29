e377ec43f0984ffdfa6821a26364503358d65342
Sadiq.Farah <sfartimyare@yahoo.com>
2022-11-28 23:11:13 -0600

    Working midway through Javascript fundamentals part 1

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -19,3 +19,101 @@
 // console.log(typeof isIsland);
 // console.log(isIsland);
 // console.log(typeof language);
+
+// CHALLENGE code exercise! (1)
+// const markWeight = 78;
+// const markHeight = 1.69;
+
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+
+// // BMI = mass / (height * weight)
+// let markBmi = markWeight / markHeight ** 2;
+// let johnBmi = johnWeight / johnHeight ** 2;
+
+// markHigherBMI = markBmi > johnBmi;
+
+// console.log(markBmi);
+// console.log(johnBmi);
+// console.log(markHigherBMI);
+
+// // SECOND DATA FOR FIRST CHALLENGE EXERCISE
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMImark = massMark / heightMark ** 2;
+// const BMIjohn = massJohn / (heightJohn * heightJohn);
+
+// if (BMImark > BMIjohn) {
+//   console.log(`Mark's BMI ${BMImark} is higher then Jon's ${BMIjohn}!`);
+// } else {
+//   console.log(`John's BMI ${BMIjohn} is higher then Mark's!${BMImark}`);
+// }
+
+// console.log(BMImark);
+// console.log(BMIjohn);
+
+/* IF ELSE STATEMENTS WELL MADE */
+// let scoreDolphin = (97 + 112 + 101) / 3;
+// let scoreKoala = (109 + 95 + 106) / 3;
+// console.log(scoreDolphin, scoreKoala);
+
+// if (scoreDolphin > scoreKoala && scoreDolphin >= 100) {
+//   console.log("Dolphins win the trophy!!");
+// } else if (scoreKoala > scoreDolphin && scoreKoala >= 100) {
+//   console.log(`Koalas win the Trophy!`);
+// } else if (
+//   scoreDolphin === scoreKoala &&
+//   scoreKoala > 100 &&
+//   scoreDolphin > 100
+// ) {
+//   console.log(`Its a Draw. Both teams win!`);
+// } else {
+//   console.log(`NOBODY WINS`);
+// }
+
+/* SWITCH EXAMPLE DOWN BELOW */
+// const day = "monday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("plan course structure");
+//     console.log("got to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("prepare Theory Videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("write code examples");
+//     break;
+//   case "friday":
+//     console.log("record Videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Its the weekend. Time to relax!");
+//     break;
+//   default:
+//     console.log("pick a valid day please!");
+//     break;
+// }
+
+// const day = "sunday";
+
+// if (day === "monday") {
+//   console.log("plan course structure");
+//   console.log("go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("write code examples");
+// } else if (day === "friday") {
+//   console.log("Record Videos!");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("its the WEEEKEND!!");
+// } else {
+//   console.log("not a valid day!");
+// }
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -109,8 +109,274 @@ OPERATOR === ALLOWS US TO COMBINE MULTIPLE VALUES AND DO ALL KIND OF WORK WITH V
 
 // console.log(now - 1991 > now - 2018);
 
-const now = 2037;
-const ageJonah = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
-console.log(now - 1991, now - 2018);
+// const now = 2037;
+// const ageJonah = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
+// console.log(now - 1991, now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // y = 10 x = 10
+
+// console.log(x, y);
+
+// const averageAge = ageJonah + ageSarah / 2;
+// console.log(ageJonah, ageSarah, averageAge);
+
+// const firstName = "jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(jonas);
+// // This is a pain way to do it!!
+// /* BETTER METHOD IS TEMPLATE STRINGS */
+
+// const jonasNew = `I'm ${firstName} a ${year - birthYear} year old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`WE can use back ticks for any regular strings`);
+
+// console.log("string with \n\
+// multiple\n\
+// lines");
+// // USING TEMPLATE STRING FOR MULTI-LINE STRING!
+// console.log(`String
+// Multiple
+// line!`);
+
+/* IF ELSE STATEMENTS! ---Control Structure*/
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("sarah can start driving license!");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`sarah is too young. wait another ${yearsLeft} years`);
+// }
+
+// const birthYear = 1999;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+/* TYPE CONVERSION/ TYPE COERCION! */
+// Type Conversion Below
+// const inputYear = `1991`;
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number(`jonas`));
+// console.log(typeof NaN); //Not a Number
+// console.log(String(23), 23);
+
+// // Type Coercion!
+// console.log(`I am ` + 23 + ` years old`); //Java does this automatically behind the scenes!
+// console.log(`23` + `10` - 3);
+// // the MINUS operator js converted string to numbers above!
+// console.log(`23` * `2`);
+// let n = `1` + 1; //11
+// n -= 1;
+// console.log(n);
+
+/* TRUTHY AND FALSY VALUES */
+
+// FIVE FALSY VALUES:
+//  0, '', undefined, null, NaN === Will become false when converted to a Boolean!
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(`jonas`));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+// console.log(Boolean(``));
+
+// let sarahName = "row";
+// if (sarahName) {
+//   console.log(`Her name is ${sarahName}`);
+// } else {
+//   console.log("that is not her name!");
+// }
+
+// let money = 0;
+// if (money >= 1) {
+//   console.log("you got some Bread! go spend it!");
+// } else {
+//   console.log("go get a job:(");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("height is defined");
+// } else {
+//   console.log("height not defined!");
+// }
+
+/* EQUALITY OPERATORS == vs === */
+// const age = `18`;
+// const age = 18;
+// if (age === 18) console.log("you just became an adult(strict)");
+// if (age == 18) console.log("you just became an adult(loose)");
+// // ALWAYS USE STRICT EQUALITY ===
+
+// const favorite = Number(prompt("what's your favorite number?"));
+// console.log(favorite, typeof favorite);
+
+// if (favorite === 23) {
+//   console.log("cool 23 is an amaizng number");
+// } else if (favorite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favorite === 9) {
+//   console.log("9 is an awesome number");
+// } else {
+//   console.log("number is not 23, 9 or 7");
+// }
+// //Diference operator !==
+// if (favorite !== 23) console.log("why not 23?");
+
+// /* BOOLEAN LOGIC = THE AND OR & NOT OPERATORS */
+
+// const hasDriverLicense = true; //Variable A
+// const hasGoodVision = true; //Variable B
+
+// console.log(hasDriverLicense && hasGoodVision);
+// console.log(hasDriverLicense || hasGoodVision);
+// console.log(!hasDriverLicense);
+
+// // if (hasDriverLicense && hasGoodVision) {
+// //   console.log("Sarah able to drive");
+// // } else {
+// //   console.log("someone else should drive...");
+// // }
+
+// const isTired = false; //Variable C
+// console.log(hasDriverLicense && hasGoodVision && isTired);
+
+// if (hasDriverLicense && hasGoodVision && !isTired) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("someone else should drive...");
+// }
+
+// const averageScoreDolphins = (96 + 108 + 89) / 3;
+// const averageScoreKoalas = (88 + 91 + 110) / 3;
+// console.log(averageScoreKoalas);
+// console.log(averageScoreDolphins);
+
+// let draw = averageScoreDolphins === averageScoreKoalas;
+
+// if (averageScoreDolphins > averageScoreKoalas) {
+//     console.log(`dolphins win the trophy!`)
+
+// } else if (averageScoreKoalas > averageScoreDolphins) {
+//     console.log(`Koalas win the Trophy!`)
+// } else if (averageScoreDolphins === averageScoreKoalas) {
+//   console.log("we HAVE A DRAW! Both are winnners");
+// }
+
+/* BEST IF EXAMPLE --> UnderStand This! */
+
+// const averageScoreDolphins = (97 + 112 + 80) / 3;
+// const averageScoreKoalas = (109 + 95 + 50) / 3;
+// console.log(averageScoreDolphins, averageScoreKoalas);
+
+// if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
+//   console.log(`dolphins win the trophy!`);
+// } else if (
+//   averageScoreKoalas > averageScoreDolphins &&
+//   averageScoreKoalas >= 100
+// ) {
+//   console.log(`Koalas win the Trophy!`);
+// } else if (
+//   averageScoreDolphins === averageScoreKoalas &&
+//   averageScoreDolphins >= 100 &&
+//   averageScoreKoalas >= 100
+// ) {
+//   console.log("we HAVE A DRAW! Both are winnners");
+// } else {
+//   console.log(`nobody is a winner!`);
+// }
+
+// const days = "sunday";
+
+// switch (days) {
+//   case "monday": //day === `monday
+//     console.log("plan course structure");
+//     console.log("go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("write code examples");
+//     break;
+//   case "friday":
+//     console.log("record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("enjoy the WEEKEND:D");
+//     break;
+//   default:
+//     console.log("Not a valid daay!");
+// }
+
+// const day = "sunday";
+
+// if (day === "monday") {
+//   console.log("plan course structure");
+//   console.log("go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("write code examples");
+// } else if (day === "friday") {
+//   console.log("Record Videos!");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("its the WEEEKEND!!");
+// } else {
+//   console.log("not a valid day!");
+// }
+
+/* DIFFERENCE BETWEEN STATEMENTS AND EXPRESSIONS */
+// EXPRESSION=== A PIECE OF CODE THAT PRODUCES A VALUE.
+// STATEMENTS=== FULL SENTENCES THAT TRANSLATE ACTIONS we want a program to perform like if else statement.
+
+// 3 + 4;
+// 1991;
+// true && false && !false; //ALL EXPRESSIONS
+
+// if (23 > 10) {
+//   //STATEMENT else if
+//   const str = "23 is bigger";
+// }
+
+// console.log(`i'm ${2037 - 1991} years old`);
+
+/* CONDITIONAl(ternary) OPERATOR */
+//another method to riding condition other then SWITCH and if else statements
+
+// const age = 9;
+// // age >= 18
+// //   ? console.log("I like to drink wine")
+// //   : console.log(`I like to drink water`);
+
+// /* HAS 3 parts!  (1)condition (2)IF (3)ELSE  */
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "wine"; //Transform big block of code to small
+// } else {
+//   drink2 = "water";
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
